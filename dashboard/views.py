e46092168e559c3a49ba1430a822fd3240934831
ratchek <git@ratchek.com>
2023-05-27 10:43:01 -0400

    Add view to display landlords

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from . import nycdb
 
 
 def index(request):
     return HttpResponse("Hello, world. This is gonna be a dashboard.")
+
+
+def landlords(request):
+    landlords = nycdb.get_landlords("74th street", "2150")
+    return render(request, "door2door/landlord.html", {"landlords": landlords})
