e94837097a0b5176e70d0e40e7090ae63dd4a17b
ratchek <git@ratchek.com>
2023-09-07 13:40:43 -0400

    Add FAQ

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("address-info", views.address_info, name="address-info"),
+    path(
+        "faq/",
+        login_required(TemplateView.as_view(template_name="door2door/faq.html")),
+        name="faq",
+    ),
+    path("address-info/", views.address_info, name="address-info"),
     re_path(
         r"^address-info/(?P<house_number>[-\w ]+)/(?P<street_name>[-\w ]+)$",
         views.address_info,
