ac4838ba23431053533eb162290e407e5e28a136
ratchek <git@ratchek.com>
2023-10-06 13:49:28 -0400

    Buttons proof of concept

---
+++ static/js/buttons.js
@@ -0,0 +1,15 @@
+function hi() {
+  alert("hi-hi");
+}
+
+function changeForm() {
+  document.getElementById("id_address-house_number").value = "My value";
+}
+
+function up() {
+  document.getElementById("id_address-house_number").value = "5";
+}
+
+function down() {
+  document.getElementById("id_address-house_number").value = "1";
+}
