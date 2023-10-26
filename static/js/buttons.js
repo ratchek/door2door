632b2f0ff34fc94e7612ebf48d93d6f1f2cdebb9
ratchek <git@ratchek.com>
2023-10-26 17:45:57 -0400

    Refactor buttons code

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -7,15 +7,25 @@ function changeForm() {
 }
 
 function up() {
-  initial_value = parseInt(
-    document.getElementById("id_address-house_number").value
-  );
-  document.getElementById("id_address-house_number").value = initial_value + 2;
+  updtateHouseNumber(2);
 }
 
 function down() {
-  initial_value = parseInt(
-    document.getElementById("id_address-house_number").value
-  );
-  document.getElementById("id_address-house_number").value = initial_value - 2;
+  updtateHouseNumber(-2);
+}
+
+function updtateHouseNumber(val) {
+  // Change so that stuff like 36-63 works as expected (you'll need to extract the number, increment it, then put it back in.)
+  // Right now the if statement is creating an endless 0 when 0 happens to be the number. Fix that
+  var new_value;
+  var numbers = document.getElementById("id_address-house_number").value;
+  var initial_value = parseInt(numbers);
+  if (initial_value) {
+    new_value = initial_value + val;
+  } else {
+    new_value = initial_value;
+  }
+  document.getElementById("id_address-house_number").value = new_value;
+  console.log("initial value = ", initial_value);
+  console.log("new value = ", new_value);
 }
