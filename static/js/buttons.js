421bfbdc593d1f8c4701c5507453a67fce7faf3e
ratchek <git@ratchek.com>
2023-10-27 11:42:11 -0400

    Update up/down button logic

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,11 +1,3 @@
-function hi() {
-  alert("hi-hi");
-}
-
-function changeForm() {
-  document.getElementById("id_address-house_number").value = "My value";
-}
-
 function up() {
   updtateHouseNumber(2);
 }
@@ -18,14 +10,31 @@ function updtateHouseNumber(val) {
   // Change so that stuff like 36-63 works as expected (you'll need to extract the number, increment it, then put it back in.)
   // Right now the if statement is creating an endless 0 when 0 happens to be the number. Fix that
   var new_value;
-  var numbers = document.getElementById("id_address-house_number").value;
-  var initial_value = parseInt(numbers);
-  if (initial_value) {
-    new_value = initial_value + val;
+  // Get the value entered into the house_number field
+  var house_number = document.getElementById("id_address-house_number").value;
+  // Get the last number that occurs (for example for 23-15N, this will return 15)
+  var just_numbers = house_number.match(/\d+/g);
+  // Make sure there's some numbers in the string!
+  if (just_numbers != null) {
+    var old_number = just_numbers.pop();
+    var new_number = parseInt(old_number) + val;
+    if (new_number < 0) {
+      alert(
+        "I don't know how to change that house number!\nPlease input it by hand."
+      );
+    } else {
+      // Insert the new (increased or decreased) number into the full house number.
+      // So if I'm increasing 23-18N, I should get 23-20N
+      var new_house_number = house_number.replace(
+        /\d+(\D*)$/,
+        new_number + "$1"
+      );
+      document.getElementById("id_address-house_number").value =
+        new_house_number;
+    }
   } else {
-    new_value = initial_value;
+    alert(
+      "I don't know how to change that house number!\nPlease input it by hand."
+    );
   }
-  document.getElementById("id_address-house_number").value = new_value;
-  console.log("initial value = ", initial_value);
-  console.log("new value = ", new_value);
 }
